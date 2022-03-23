3cffc5c053024e8d581bc6ac6e78d7b29f60b500
justincasecmt <101856213+justincasecmt@users.noreply.github.com>
2022-03-23 14:36:42 +0800

    Add files via upload

--- Just In Case/Just In Case/ViewController.swift
+++ Just In Case/Just In Case/ViewController.swift
@@ -9,11 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var collectionview: UICollectionView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+     
     }
-
-
 }
+
 
---
+++ Just In Case/Just In Case/meun/seafood.swift
@@ -0,0 +1,27 @@
+//
+//  seafood.swift
+//  Just In Case
+//
+//  Created by elvatsai on 23/3/2022.
+//
+
+import Foundation
+
+class seafood {
+    var title: String
+    var image: String
+    var price: String
+    init(cPic:String,)
+
+
+let seafoods: [Seafood ] = [
+    Seafood (title: "Norway Salmon Steak", image: #imageLiteral(resourceName: "Norway Salmon Steak"), price: "HKD $70"),
+    Seafood (title: "Frozen Shrimp", image: #imageLiteral(resourceName: "SHRIMP"),price: "HKD $75"),
+    Seafood (title: "American Lobster", image: #imageLiteral(resourceName: "American lobster"),price: "HKD $150"),
+    Seafood (title: " Frozen Bay Scallop", image: #imageLiteral(resourceName: "Sashimi"),price: "HKD $100"),
+    Seafood (title: "Frozen  Octopus", image: #imageLiteral(resourceName: "Octopus"),price: "HKD $100"),
+    Seafood (title: "Frozen Greenland  Halibut Steaks", image: #imageLiteral(resourceName: "halibut steaks"),price: "HKD $50"),
+    Seafood (title: "New Zealand Half Shell Mussels", image: #imageLiteral(resourceName: "NEW ZEALAND HALF SHELL MUSSELS"),price: "HKD $48"),
+    Seafood (title: "Chilled Yellow Snubosepompano", image: #imageLiteral(resourceName: "CHILLED YELLOW SNUBNOSEPOMPANO"),price: "HKD $55"),
+    Seafood (title: "Pacific Saury", image: #imageLiteral(resourceName: "PACIFIC SAURY"),price: "HKD $55")
+]
